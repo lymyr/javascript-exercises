50c4f1d1347e098e7264ea861a82362d2254283f
lymyr <176290600+lymyr@users.noreply.github.com>
2025-02-19 09:26:18 +0800

    did exercises 6-8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let n = numArr.length;
+  let sum = 0;
+  if (n == 0) {
+    return sum
+  }
+  for (let i = 0; i < n; i++) {
+    sum += numArr[i];
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let n = numArr.length;
+  let sum = 1;
+  if (n == 0) {
+    return sum
+  }
+  for (let i = 0; i < n; i++) {
+    sum *= numArr[i];
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let output = 1;
+	if (x == 0 || x == 1) {
+    return output
+  }
+  else {
+    for (let i = 1; i <= x; i++) {
+      output *= i;
+    }
+  }
+  return output
 };
 
 // Do not edit below this line
