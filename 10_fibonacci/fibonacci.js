16722e66b664d9b2d33528415e1ef6cfdc2d60dd
lymyr <176290600+lymyr@users.noreply.github.com>
2025-02-19 10:43:07 +0800

    Finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS"
+    }
 
+    num = parseInt(num);
+    let fib = 0;
+    let prev = 1;
+    let prevprev = 0;
+    for (let i = 0; i < num; i++) {
+        prevprev = prev
+        prev = fib
+        fib = prevprev + prev;
+    }
+    return fib
 };
 
 // Do not edit below this line
