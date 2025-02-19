f51fa1ce295dc5c5ab8333e9f85ced8a0d2682f5
lymyr <176290600+lymyr@users.noreply.github.com>
2025-02-19 10:34:06 +0800

    finished palindrome exexercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (inputStr) {
+    inputStr = inputStr.toLowerCase();
+    inputStr = inputStr.replace(/[^a-z\d]+/g, "");
 
+    let endIndex = inputStr.length - 1;
+    for (let i = 0; i < inputStr.length/2; i++) {
+        if (inputStr[i] != inputStr[endIndex]) {
+            return false;
+        }
+        endIndex = endIndex - 1;
+    }
+    return true
 };
 
 // Do not edit below this line
