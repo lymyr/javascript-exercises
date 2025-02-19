844dd539cadab2b3c03245171bc93cd705924a52
lymyr <176290600+lymyr@users.noreply.github.com>
2025-02-19 10:52:56 +0800

    finished get titles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrObj) {
+    let titles = [];
+    for (let i = 0; i < arrObj.length; i++) {
+        titles.push(arrObj[i].title)
+    }
+    return titles
+    // let titles = [];
+    // for (title in books.titles) {
+    //     title 
+    // }
 };
 
 // Do not edit below this line
