b76eed3d5f6d84b60ffe925294e44fe0095cf4de
KnLemr <gkq1038@dlsud.edu.ph>
2025-03-09 21:03:44 +0800

    finished last exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let oldestAge = 0;
+    let oldestPerson = "omegalul";
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    for (let i = 0; i < obj.length; i++) {
+        let yod = obj[i].yearOfDeath;
+        if (obj[i].yearOfDeath == undefined) {
+            yod = currentYear;
+        }
 
+        let age = yod - obj[i].yearOfBirth;
+        if (age > oldestAge) {
+            oldestAge = age;
+            oldestPerson = obj[i];
+        }
+    }
+    return oldestPerson
 };
 
 // Do not edit below this line
